620dd253ede2c62ca0505f49fcf7e3ffe2e5ff4e
Grant Kelsay <62901403+grantkelsay@users.noreply.github.com>
2023-11-02 11:14:05 -0700

    Update KanbanBoard.tsx

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -241,8 +241,8 @@ function KanbanBoard() {
                 </DragOverlay>, document.body)}
 
             </DndContext>
-            <div className="flex items-center text-center">
-                <p>Created By</p>
+            <div className="flex items-center text-center p-10 opacity-60">
+                <p>Created By</p><br/>
                 <p>Grant Kelsay | github.com/grantkelsay | linkedin.com/in/grant-kelsay </p>
             </div>
         </div>

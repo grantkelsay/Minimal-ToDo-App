c9904cca04ba156ae2c24a17453889eeac19ef1d
Grant Kelsay <grantkelsay@gmail.com>
2023-11-08 20:15:17 -0800

    Creating CRUD operations for columns and tasks

--- backEnd/src/main/java/com/minimal/minimalTodo/controller/ColumnsController.java
+++ backEnd/src/main/java/com/minimal/minimalTodo/controller/ColumnsController.java
@@ -1,26 +1,94 @@
 package com.minimal.minimalTodo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.minimal.minimalTodo.model.Columns;
 import com.minimal.minimalTodo.repository.ColumnsRepository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ColumnsController {
     
-    private final ColumnsRepository todoColsRepository;
+    private final ColumnsRepository colsRepo;
 
     @Autowired
-    public ColumnsController(ColumnsRepository todoColsRepository) {
-        this.todoColsRepository = todoColsRepository;
+    public ColumnsController(ColumnsRepository colsRepo) {
+        this.colsRepo = colsRepo;
     }
 
+    // Return the list of columns stored in the H2 database
     @GetMapping("/columns")
-    public List<Columns> columns() {
-        return todoColsRepository.findAll();
+    public ResponseEntity<List<Columns>> getAllColumns() {
+        try {
+            List<Columns> columnList = new ArrayList<>();
+            colsRepo.findAll().forEach(columnList::add);
+
+            // If there are no columns in the database
+            //  return NO_CONTENT
+            if (columnList.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            // Else return 200
+            return ResponseEntity.ok(columnList);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/addColumn")
+    public ResponseEntity<Columns> addColumn(@RequestBody Columns column) {
+        Columns columnObj = colsRepo.save(column);
+
+        return new ResponseEntity<>(columnObj, HttpStatus.OK);
+    }
+
+    @PostMapping("/updateColumnById/{id}")
+    public ResponseEntity<Columns> updateColumnById(@PathVariable Integer id, @RequestBody Columns newColumnData) {
+
+        // Retrieve the column using its id
+        Optional<Columns> oldColumnData = colsRepo.findById(id);
+
+        // If the column is found by its id
+        if (oldColumnData.isPresent()) {
+
+            // Create a clone of the column and store the new details inside of the copy
+            Columns updatedColumnData = oldColumnData.get();
+            updatedColumnData.setTitle(newColumnData.getTitle());
+            updatedColumnData.setIsNew(newColumnData.getIsNew());
+
+            // Create a new columns object and save it to the repo
+            Columns columnObj = colsRepo.save(updatedColumnData);
+            return new ResponseEntity<>(columnObj, HttpStatus.OK);
+        }
+
+        // Else return 404
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/deleteColumnById/{id}")
+    public ResponseEntity<Columns> deleteColumnById(@PathVariable Integer id) {
+
+        // If the column to be deleted is found by its id
+        if (colsRepo.findById(id) != null) {
+            colsRepo.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        
+        // Else return 404
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        
     }
 }
--- backEnd/src/main/java/com/minimal/minimalTodo/controller/TasksController.java
+++ backEnd/src/main/java/com/minimal/minimalTodo/controller/TasksController.java
@@ -1,27 +1,98 @@
 package com.minimal.minimalTodo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.minimal.minimalTodo.model.Tasks;
 import com.minimal.minimalTodo.repository.TasksRepository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TasksController {
     
-    private final TasksRepository todoTasksRepository;
+    private final TasksRepository taskRepo;
 
     @Autowired
-    public TasksController(TasksRepository todoTasksRepository) {
-        this.todoTasksRepository = todoTasksRepository;
+    public TasksController(TasksRepository taskRepo) {
+        this.taskRepo = taskRepo;
     }
 
+
+    // Return the list of tasks stored in the H2 database
     @GetMapping("/tasks")
-    public List<Tasks> tasks() {
-        return todoTasksRepository.findAll();
+    public ResponseEntity<List<Tasks>> getAllTasks() {
+        try {
+            List<Tasks> taskList = new ArrayList<>();
+            taskRepo.findAll().forEach(taskList::add);
+
+            // If there are no tasks in the database
+            //  return NO_CONTENT
+            if (taskList.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            // Else return 200
+            return new ResponseEntity<>(taskList, HttpStatus.OK);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/addTask")
+    public ResponseEntity<Tasks> addTask(@RequestBody Tasks task) {
+        Tasks taskObj = taskRepo.save(task);
+
+        return new ResponseEntity<>(taskObj, HttpStatus.OK);
+    }
+
+        @PostMapping("/updateTaskById/{id}")
+    public ResponseEntity<Tasks> updateTaskById(@PathVariable Integer id, @RequestBody Tasks newTaskData) {
+
+        // Retrieve the column using its id
+        Optional<Tasks> oldTaskData = taskRepo.findById(id);
+
+        // If the column is found by its id
+        if (oldTaskData.isPresent()) {
+
+            // Create a clone of the column and store the new details inside of the copy
+            Tasks updateTaskData = oldTaskData.get();
+            updateTaskData.setColumnId(newTaskData.getColumnId());
+            updateTaskData.setContent(newTaskData.getContent());
+            updateTaskData.setColor(newTaskData.getColor());
+            updateTaskData.setIsNew(newTaskData.getIsNew());
+
+            // Create a new columns object and save it to the repo
+            Tasks taskObj = taskRepo.save(updateTaskData);
+            return new ResponseEntity<>(taskObj, HttpStatus.OK);
+        }
+
+        // Else return 404
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/deleteTaskById/{id}")
+    public ResponseEntity<Tasks> deleteTaskById(@PathVariable Integer id) {
+
+        // If the column to be deleted is found by its id
+        if (taskRepo.findById(id) != null) {
+            taskRepo.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        
+        // Else return 404
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        
     }
 
 }
